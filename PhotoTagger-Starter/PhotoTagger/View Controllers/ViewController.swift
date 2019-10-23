8601e1b42ac6900212a35adae14a524d0680742b
edu <vizuete.eduardo@gmail.com>
2019-10-23 16:17:47 -0500

    Alamofire: upload image with imagga API

--- PhotoTagger-Starter/PhotoTagger/View Controllers/ViewController.swift
+++ PhotoTagger-Starter/PhotoTagger/View Controllers/ViewController.swift
@@ -25,83 +25,134 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
-
-import UIKit
+import Alamofire
 import SwiftyJSON
-
+import UIKit
 class ViewController: UIViewController {
-
   // MARK: - IBOutlets
   @IBOutlet var takePictureButton: UIButton!
   @IBOutlet var imageView: UIImageView!
   @IBOutlet var progressView: UIProgressView!
   @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
-
   // MARK: - Properties
   private var tags: [String]?
   private var colors: [PhotoColor]?
-
   // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
-
     if !UIImagePickerController.isSourceTypeAvailable(.camera) {
       takePictureButton.setTitle("Select Photo", for: .normal)
     }
   }
-
+  
   override func viewDidDisappear(_ animated: Bool) {
     super.viewDidDisappear(animated)
-
     imageView.image = nil
   }
-
+  
   // MARK: - Navigation
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
     if segue.identifier == "ShowResults",
       let controller = segue.destination as? TagsColorsViewController {
       controller.tags = tags
       controller.colors = colors
     }
   }
-
+  
   // MARK: - IBActions
   @IBAction func takePicture(_ sender: UIButton) {
     let picker = UIImagePickerController()
     picker.delegate = self
     picker.allowsEditing = false
-
     if UIImagePickerController.isSourceTypeAvailable(.camera) {
       picker.sourceType = .camera
     } else {
       picker.sourceType = .photoLibrary
       picker.modalPresentationStyle = .fullScreen
     }
-
     present(picker, animated: true)
   }
+  
 }
-
 // MARK: - UIImagePickerControllerDelegate
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
     guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
       print("Info did not have the required UIImage for the Original Image")
       dismiss(animated: true)
       return
     }
-
     imageView.image = image
-
     dismiss(animated: true)
+    // 1
+    takePictureButton.isHidden = true
+    progressView.progress = 0.0
+    progressView.isHidden = false
+    activityIndicatorView.startAnimating()
+    upload(
+      image: image,
+      progressCompletion: { [weak self] percent in
+        // 2
+        self?.progressView.setProgress(percent, animated: true)
+      },
+      completion: { [weak self] tags, colors in
+        // 3
+        self?.takePictureButton.isHidden = false
+        self?.progressView.isHidden = true
+        self?.activityIndicatorView.stopAnimating()
+        self?.tags = tags
+        self?.colors = colors
+        // 4
+        self?.performSegue(withIdentifier: "ShowResults", sender: self)
+    })
   }
+  
 }
-
 extension ViewController {
   func upload(image: UIImage,
               progressCompletion: @escaping (_ percent: Float) -> Void,
               completion: @escaping (_ tags: [String]?, _ colors: [PhotoColor]?) -> Void) {
+    // 1
+    guard let imageData = UIImageJPEGRepresentation(image, 0.5) else {
+      print("Could not get JPEG representation of UIImage")
+      return
+    }
+    // 2
+    Alamofire.upload(
+      multipartFormData: { multipartFormData in
+        multipartFormData.append(
+          imageData,
+          withName: "image",
+          fileName: "image.jpg",
+          mimeType: "image/jpeg")
+      },
+      to: "https://api.imagga.com/v2/uploads",
+      headers: ["Authorization": "Basic yxz"],
+      encodingCompletion: { encodingResult in
+        switch encodingResult {
+          case .success(let upload, _, _):
+            upload.uploadProgress { progress in
+              progressCompletion(Float(progress.fractionCompleted))
+            }
+            upload.validate()
+            upload.responseJSON { response in
+              // 1
+              guard response.result.isSuccess,
+                let value = response.result.value else {
+                  print("Error while uploading file: \(String(describing: response.result.error))")
+                  completion(nil, nil)
+                  return
+              }
+              // 2
+              let firstFileID = JSON(value)["result"]["upload_id"].stringValue
+              print("Content uploaded with ID: \(firstFileID)")
+              // 3
+              completion(nil, nil)
+            }
+          case .failure(let encodingError):
+            print(encodingError)
+        }
+    })
   }
+  
 }
--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# mobile_ios_swinject
-Swinject Tutorial for iOS: Getting Started
-  - https://www.raywenderlich.com/17-swinject-tutorial-for-ios-getting-started
-Alamofire Tutorial: Getting Started
+# Alamofire Tutorial: Getting Started
   - https://www.raywenderlich.com/35-alamofire-tutorial-getting-started
-  
+  - https://gist.github.com/gkostadinov/2c53453eda32c6730acc64e9784a9c95  
